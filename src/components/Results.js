373a168a578214b18658c8fbdedc80a475239c16
Harsh Darji <harshdarji1995@gmail.com>
2020-04-20 17:04:10 +0530

    result design update

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,35 +1,42 @@
 import React from 'react';
 import {get} from 'lodash'
 
+import './Results.scss'
+
 const getRiskType = (risk) => {
-    if(risk === 1) return ['low', 'lowRecommendations']
-    else if(risk === 2) return ['medium', 'mediumRecommendations']
-    else if(risk === 3) return ['high', 'highRecommendations']
+	if(risk === 1) return ['low', 'lowRecommendations']
+	else if(risk === 2) return ['medium', 'mediumRecommendations']
+	else if(risk === 3) return ['high', 'highRecommendations']
 }
 
 export default ({data, risk}) => {
 
-    const [risk_type, risk_recom] = getRiskType(risk)
-    const recom = get(data, risk_recom, {})
-    
-    return (
-        <div id="results">
-            <div className='header'>
-                {data.header_title} : <b>{data[risk_type]}</b>
-            </div>
+	const [risk_type, risk_recom] = getRiskType(risk)
+	const recom = get(data, risk_recom, {})
+	
+	return (
+		<div id="results">
+			<div className='header'>
+				<div className="header-content">
+					<div className="content left">{data.header_title}</div>
+					<div className={`content right ${risk_type}`}>{data[risk_type]}</div>
+				</div>
+			</div>
 
-            <div className='recom-wrapper'>
-                <div className='recom-title'>
-                    {recom.title}
-                </div>
-                {recom.list.map((item, ind)=> {
-                    return (
-                        <div className='recom-content' key={ind}>
-                            {item}
-                        </div>
-                    )
-                })}
-            </div>
-        </div>
-    )
+			<div className='recom-wrapper'>
+				<div className='recom-title'>
+					{recom.title}
+				</div>
+				
+				{recom.list.map((item, ind)=> {
+					return (
+						<div className='recom-content' key={ind}>
+							<div>{ind+1}.</div>
+							<div dangerouslySetInnerHTML={{__html : item }} />
+						</div>
+					)
+				})}
+			</div>
+		</div>
+	)
 }
